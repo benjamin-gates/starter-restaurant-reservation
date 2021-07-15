779f715d41ac6b4cc384faafebd44900ecbe12d1
Ben Gates <benjamin_gates@outlook.com>
2021-07-15 07:25:00 -0600

    added finish button to occupied tables

--- front-end/src/dashboard/ListTables.js
+++ front-end/src/dashboard/ListTables.js
@@ -5,14 +5,19 @@ function ListTables({ tables }) {
   let backgroundColor = null;
   return tables.map((table) => {
     if (table.reservation_id) {
-        backgroundColor = "lightred";
+      backgroundColor = "lightred";
       status = (
-        <div data-table-id-status={table.table_id} style={{ color: "red" }}>
-          <strong>Occupied</strong>
+        <div style={{display: "flex", flexDirection: "row", justifyContent: "space-between"}}>
+          <div data-table-id-status={table.table_id} style={{ color: "red" }}>
+            <strong>Occupied </strong>
+          </div>
+          <button type="button" className="btn btn-secondary btn-outline-light">
+            Finish
+          </button>
         </div>
       );
     } else {
-        backgroundColor = "#C0E9C6";
+      backgroundColor = "#C0E9C6";
       status = (
         <div data-table-id-status={table.table_id} style={{ color: "green" }}>
           <strong>Free</strong>
@@ -20,7 +25,11 @@ function ListTables({ tables }) {
       );
     }
     return (
-      <li key={table.table_id} className="list-group-item" style={{backgroundColor: backgroundColor}}>
+      <li
+        key={table.table_id}
+        className="list-group-item"
+        style={{ backgroundColor: backgroundColor }}
+      >
         {status}
         <div>
           Table: {table.table_name} | Capacity: {table.capacity}
