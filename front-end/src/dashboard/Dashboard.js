6e1c3b6f78c57c93fe875de3bb9df75c2a8afe8d
Ben Gates <benjamin_gates@outlook.com>
2021-07-22 11:38:17 -0600

    moved routing for NewReservation from Dashboard component to Routes component

--- front-end/src/dashboard/Dashboard.js
+++ front-end/src/dashboard/Dashboard.js
@@ -6,7 +6,6 @@ import { previous, next } from "../utils/date-time";
 import ListReservations from "./ListReservations";
 import ListTables from "./ListTables";
 import EditReservation from "../reservations/EditReservation";
-import NewReservation from "../reservations/NewReservation";
 import SeatReservation from "../reservations/SeatReservation";
 
 /**
@@ -31,113 +30,114 @@ function Dashboard({ date }) {
     listTables(abortController.signal).then(setTables).catch(setTablesError);
     return () => {
       setReservations(undefined);
-      abortController.abort()};
+      abortController.abort();
+    };
   }
   return reservations ? (
     <Switch>
       <Route path="/reservations/:reservation_id/edit">
-        <EditReservation
-          setReservations={setReservations}
-        />
+        <EditReservation setReservations={setReservations} />
       </Route>
       <Route path="/reservations/:reservation_id/seat">
         <SeatReservation />
       </Route>
-      <Route path="/reservations/new">
-        <NewReservation />
-      </Route>
-      <Route path="/">
-      <main>
-        <h1>Dashboard</h1>
-        <div style={{ textAlign: "center" }}>
-          <div className="btn-group" role="group" aria-label="dashboard-group">
-            <button
-              type="button"
-              className="btn btn-dark btn-outline-light"
-              onClick={() => {
-                setReservations(undefined);
-                history.push(`/dashboard?date=${previous(date)}`);
-              }}
-            >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="16"
-                height="16"
-                fill="currentColor"
-                className="bi bi-arrow-left-short"
-                viewBox="0 0 16 16"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M12 8a.5.5 0 0 1-.5.5H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5a.5.5 0 0 1 .5.5z"
-                />
-              </svg>
-              Previous
-            </button>
-            <button
-              type="button"
-              className="btn btn-dark btn-outline-light"
-              onClick={() => {
-                setReservations(undefined);
-                history.push("/dashboard");
-              }}
-            >
-              Today
-            </button>
-            <button
-              type="button"
-              className="btn btn-dark btn-outline-light"
-              onClick={() => {
-                setReservations(undefined);
-                history.push(`/dashboard?date=${next(date)}`);
-              }}
-            >
-              Next
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="16"
-                height="16"
-                fill="currentColor"
-                className="bi bi-arrow-right-short"
-                viewBox="0 0 16 16"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M4 8a.5.5 0 0 1 .5-.5h5.793L8.146 5.354a.5.5 0 1 1 .708-.708l3 3a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708-.708L10.293 8.5H4.5A.5.5 0 0 1 4 8z"
-                />
-              </svg>
-            </button>
-          </div>
-        </div>
 
-        <ErrorAlert error={reservationsError} />
-        <div className="dashboard-lists">
-          <section className="container-fluid">
-            <div className="container-fluid dashboard-header">
-              <h4 className="mb-0">Reservations for date: {date}</h4>
+      <Route path="/">
+        <main>
+          <h1>Dashboard</h1>
+          <div style={{ textAlign: "center" }}>
+            <div
+              className="btn-group"
+              role="group"
+              aria-label="dashboard-group"
+            >
+              <button
+                type="button"
+                className="btn btn-dark btn-outline-light"
+                onClick={() => {
+                  setReservations(undefined);
+                  history.push(`/dashboard?date=${previous(date)}`);
+                }}
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="16"
+                  height="16"
+                  fill="currentColor"
+                  className="bi bi-arrow-left-short"
+                  viewBox="0 0 16 16"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M12 8a.5.5 0 0 1-.5.5H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5a.5.5 0 0 1 .5.5z"
+                  />
+                </svg>
+                Previous
+              </button>
+              <button
+                type="button"
+                className="btn btn-dark btn-outline-light"
+                onClick={() => {
+                  setReservations(undefined);
+                  history.push("/dashboard");
+                }}
+              >
+                Today
+              </button>
+              <button
+                type="button"
+                className="btn btn-dark btn-outline-light"
+                onClick={() => {
+                  setReservations(undefined);
+                  history.push(`/dashboard?date=${next(date)}`);
+                }}
+              >
+                Next
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="16"
+                  height="16"
+                  fill="currentColor"
+                  className="bi bi-arrow-right-short"
+                  viewBox="0 0 16 16"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M4 8a.5.5 0 0 1 .5-.5h5.793L8.146 5.354a.5.5 0 1 1 .708-.708l3 3a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708-.708L10.293 8.5H4.5A.5.5 0 0 1 4 8z"
+                  />
+                </svg>
+              </button>
             </div>
-            <div>
-              {reservations ? (
-                <ListReservations reservations={reservations} date={date} />
-              ) : (
-                <div>No reservations are scheduled for this date.</div>
-              )}
-            </div>
-          </section>
-          <section className="container-fluid">
-            <div>
-              <h4>Tables</h4>
-              {tables.length && reservations ? (
-                <ul className="list-group" style={{ width: "30rem" }}>
-                  <ListTables tables={tables} />
-                </ul>
-              ) : (
-                <ErrorAlert error={tablesError} />
-              )}
-            </div>
-          </section>
-        </div>
-      </main>
+          </div>
+
+          <ErrorAlert error={reservationsError} />
+          <div className="dashboard-lists">
+            <section className="container-fluid">
+              <div className="container-fluid dashboard-header">
+                <h4 className="mb-0">Reservations for date: {date}</h4>
+              </div>
+              <div>
+                {reservations ? (
+                  <ListReservations reservations={reservations} date={date} />
+                ) : (
+                  <div>No reservations are scheduled for this date.</div>
+                )}
+              </div>
+            </section>
+            <section className="container-fluid">
+              <div>
+                <h4>Tables</h4>
+                {tables.length && reservations ? (
+                  <ul className="list-group" style={{ width: "30rem" }}>
+                    <ListTables tables={tables} />
+                  </ul>
+                ) : (
+                  <ErrorAlert error={tablesError} />
+                )}
+              </div>
+            </section>
+          </div>
+        </main>
       </Route>
     </Switch>
   ) : null;
--- front-end/src/layout/Routes.js
+++ front-end/src/layout/Routes.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Redirect, Route, Switch } from "react-router-dom";
 import Dashboard from "../dashboard/Dashboard";
 import NewTable from "../tables/NewTable";
+import NewReservation from "../reservations/NewReservation";
 import Search from "../search/Search";
 import NotFound from "./NotFound";
 import { today } from "../utils/date-time";
@@ -26,6 +27,9 @@ function Routes() {
       </Route>
       <Route path="/search">
         <Search />
+      </Route>
+      <Route path="/reservations/new">
+        <NewReservation />
       </Route>
       <Route path="/reservations">
         <Dashboard date={query.get("date")? query.get("date") : today()} />
